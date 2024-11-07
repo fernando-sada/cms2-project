8f11b6ec0f1a957f7e5b92bb45f51d317f0aa3ac
Nhu Ly <nhuthiquynhly@gmail.com>
2024-11-07 09:01:07 -0500

    finished contact form

--- template-parts/part-contact.php
+++ template-parts/part-contact.php
@@ -1,17 +1,30 @@
-<div class="flex center">
-    <h2>Send Us A Message</h2>
-    <div>
-        <label for="name">Tell us your name *</label>
-        <input type="text" placeholder="First name">
-        <input type="text" placeholder="Last name">
+<div>
+    <section>
+        <p><span class="material-symbols-outlined">
+            location_on</span>Address</p>
+        <p>Mada Center 8th floor, 379 Hudson St, New York, NY 10018 US</p>
+        <p><span class="material-symbols-outlined">
+            call</span>Lets Talk</p>
+        <p>+1 800 123456789</p>
+        <p><span class="material-symbols-outlined">
+            mail</span>General Support</p>
+        <p>info@newyorktourism.com</p>
+    </section>
+    <div class="flex center">
+        <h2>Send Us A Message</h2>
+        <div>
+            <label for="name">Tell us your name *</label>
+            <input type="text" placeholder="First name">
+            <input type="text" placeholder="Last name">
+        </div>
+        <div>
+            <label for="email">Enter your email *</label>
+            <input type="text" placeholder="Eg. info@newyorktourism.com">
+        </div>
+        <div>
+            <label for="message">Message *</label>
+            <textarea placeholder="Write us a message"></textarea>
+        </div>
+        <button>SEND MESSAGE</button>
     </div>
-    <div>
-        <label for="email">Enter your email *</label>
-        <input type="text" placeholder="Eg. info@newyorktourism.com">
-    </div>
-    <div>
-        <label for="message">Message *</label>
-        <textarea placeholder="Write us a message"></textarea>
-    </div>
-    <button>SEND MESSAGE</button>
 </div>
