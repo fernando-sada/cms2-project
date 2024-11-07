cff4c590f9ce371f1e8dd0571e8188bf40c0d552
Nhu Ly <nhuthiquynhly@gmail.com>
2024-11-07 08:54:24 -0500

    add contact form

--- template-parts/part-contact.php
+++ template-parts/part-contact.php
@@ -1,7 +1,17 @@
-<section class="flex center">
-    <h2>Contact Us</h2>
-    <p>Address: 124, Rockharding Ave., City</p>
-    <p>Number: +1 (234) 437 89</p>
-    <p>Email:<a href="mailto:info@newyorktourism.com">info@newyorktourism</a></p>
-    <button>Sign Up</button>
-</section>
+<div class="flex center">
+    <h2>Send Us A Message</h2>
+    <div>
+        <label for="name">Tell us your name *</label>
+        <input type="text" placeholder="First name">
+        <input type="text" placeholder="Last name">
+    </div>
+    <div>
+        <label for="email">Enter your email *</label>
+        <input type="text" placeholder="Eg. info@newyorktourism.com">
+    </div>
+    <div>
+        <label for="message">Message *</label>
+        <textarea placeholder="Write us a message"></textarea>
+    </div>
+    <button>SEND MESSAGE</button>
+</div>
